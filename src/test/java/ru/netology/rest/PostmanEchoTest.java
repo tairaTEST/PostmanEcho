5175cf73597fa36b928fa67c47ccc3def872df08
Tatiana Noskova <tnoskowa@mail.ru>
2023-12-10 12:11:34 +0500

    successful test

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -3,6 +3,7 @@ package ru.netology.rest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 
 public class PostmanEchoTest {
@@ -21,7 +22,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalto("Hellow"));
+                .body("data", equalTo("Hellow"));
     }
 
 
